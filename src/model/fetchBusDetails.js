5ac35d75078937ede0212666198293a97d73c441
Piyush <120779702+mepiyushss@users.noreply.github.com>
2024-03-11 22:35:22 +0530

    json api

--- src/model/fetchBusDetails.js
+++ src/model/fetchBusDetails.js
@@ -1,21 +1,18 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export default async function fetchBusDetails() {
+  const data = await axios
+    .get(
+      "https://raw.githubusercontent.com/mepiyushss/bus-booking-system/main/src/component/bus-data.json"
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with your fetch operation:", error);
+    });
+  const buses = data.buses;
+  console.log("data", buses);
 
-    const data= await axios.get('https://raw.githubusercontent.com/Abhi3391/react-training-project-bus-ticket-booking/main/src/component/bus-data.json')
-      .then(response => {
-        return response.data;
-      })
-      .catch(error => {
-        console.error('There was a problem with your fetch operation:', error);
-      });
-      const  buses = data.buses;
-      console.log("data",buses);
-
-
-      return buses;
-    
+  return buses;
 }
-
-
